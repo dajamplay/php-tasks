2549c9244ad7757700c20024fdaa5b071300743e
dajamplay <ggyoug@gmail.com>
2021-05-05 19:35:44 +0300

    task 5

---
+++ dir/5.php
@@ -0,0 +1,19 @@
+<?php
+
+$cols = 10;
+$rows = 10;
+
+echo '<table style="border: darkblue 1px solid; border-radius: 5px;">';
+for ($i = 1; $i < $rows + 1; $i++) {
+    echo '<tr>';
+    for ($j = 1; $j < $cols + 1; $j++) {
+        if ($i == 1 || $j == 1) {
+            echo '<td style="border: darkblue 1px solid; padding: 11px; color: bisque; background-color: chocolate; border-radius: 5px;">' . $i * $j . '</td>';
+        } else {
+            echo '<td style="border: darkblue 1px solid; padding: 11px; color: darkred; background-color: bisque; border-radius: 5px;">' . $i * $j . '</td>';
+        }
+
+    }
+    echo '</tr>';
+}
+echo '</table>';
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <a href="/dir/2.php">2</a><br />
 <a href="/dir/3.php">3</a><br />
 <a href="/dir/4.php">4</a><br />
+<a href="/dir/5.php">5</a><br />
 
 
 <?php
